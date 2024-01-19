f6a4a75567e87b7747136627f714ca9a7b511f39
Eduardo Bagarrão <general@eduardobagarrao.com>
2024-01-19 21:03:48 +0000

    Inplemented player movement

---
+++ MediaListener/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField]
+	private float _movementSpeed = 0f;
+	
+	[SerializeField]
+	private Rigidbody _rigidbody;
+
+    // Update is called once per frame
+	void FixedUpdate()
+	{
+		var torque = new Vector3(InputManager.Instance.Movement.x * _movementSpeed, 0, InputManager.Instance.Movement.y * _movementSpeed);
+		
+		_rigidbody.AddTorque(torque);
+    }
+}
